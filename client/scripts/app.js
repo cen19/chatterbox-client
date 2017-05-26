daef0347a3a961cfd02257441f562a936f44c780
Hack Reactor Student <student@hackreactor.com>
2017-05-26 16:03:03 -0700

    clearMessages working

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,6 +5,45 @@ app.init = function() {
   
 };
 
+var message = {
+  username: 'Mel Brooks',
+  text: 'It\'s good to be the king',
+  roomname: 'lobby'
+};
+
+
 app.send = function() {
+  $.ajax({
+    type: 'POST',
+    url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: null,
+  });
+};
+
+app.fetch = function () { 
+  $.ajax({
+    type: 'GET',
+    url: url,
+    
+  });
+};
+
+app.clearMessages = function() {
+  $('#chats').remove();
+};
+
+var message = {
+  username: 'Mel Brooks',
+  text: 'Never underestimate the power of the Schwartz!',
+  roomname: 'lobby'
+};
+
+app.renderMessage = function() {
+  
+};
+
+app.renderRoom = function() {
   
 };
